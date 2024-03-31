e09b331984bcdfe160e194d1c46f18ecbd0e8f98
Adhnan Jeff M S <145822540+adhnanjeff@users.noreply.github.com>
2024-03-31 19:21:12 +0530

    Delete Projects/Day 26 directory

--- Projects/Day 26/main.py
+++
@@ -1,9 +0,0 @@
-import pandas
-
-data = pandas.read_csv("Base/Projects/Day 26/nato_phonetic_alphabet.csv")
-new_dict = {row.letter: row.code for (index,row) in data.iterrows()}
-
-user = input("Enter a word: ").upper()
-
-output_list = [new_dict[char] for char in user]
-print(output_list)
